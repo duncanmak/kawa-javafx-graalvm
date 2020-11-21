f06d1d839aeb0a3243145fdea9b5107ddfc3e53d
Duncan Mak <duncanmak@gmail.com>
2020-11-20 23:11:08 -0500

    Make mvn compile work

--- src/main/scheme/main.scm
+++ src/main/scheme/main.scm
@@ -1,8 +1,8 @@
-(module-name javafx-app)
-(module-extends javafx.application.Application)
+(module-name javafx-from-kawa)
+(module-compile-options main: #t)
 
 (import (class java.lang System)
-        (class javafx.application Application)
+        (class javafx.application Platform)
         (class javafx.scene.control Button)
         (class javafx.scene.layout StackPane)
         (class javafx.scene Scene)
@@ -11,10 +11,14 @@
 (define (hello-world) (System:out:println "Hello World!"))
 
 (define (start (stage ::Stage))
-  (let* ((btn (Button text: "Say 'Hello World'" on-action: (lambda (e) (hello-world))))
-         (root (StackPane)))
+  (let ((btn  (Button text: "Say 'Hello World'"
+                      on-action: (lambda (e) (hello-world))))
+        (root (StackPane)))
     (root:children:add btn)
     (set! stage:scene (Scene root 300 250))
     (stage:show)))
 
-(Application:launch (module-class) @command-line-arguments)
+(Platform:startup
+ (lambda ()
+   (let ((stage (Stage)))
+     (start stage))))
