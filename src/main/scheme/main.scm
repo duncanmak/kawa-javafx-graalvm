c8021eed4b41401b56cf3f10864de758649e47df
Duncan Mak <duncan@yomigana>
2022-09-17 18:50:52 -0400

    Load RichTextArea

--- src/main/scheme/main.scm
+++ src/main/scheme/main.scm
@@ -1,21 +1,17 @@
 (module-name javafx-from-kawa)
 (module-compile-options main: #t)
 
-(import (class java.lang System)
+(import (class com.gluonhq.richtextarea RichTextArea)
+        (class java.lang System)
         (class javafx.application Platform)
-        (class javafx.scene.control Button)
-        (class javafx.scene.layout StackPane)
+        (class javafx.scene.layout BorderPane)
         (class javafx.scene Scene)
         (class javafx.stage Stage))
 
-(define (hello-world) (System:out:println "Hello World!"))
-
 (define (start (stage ::Stage))
-  (let ((btn  (Button text: "Say 'Hello World'"
-                      on-action: (lambda (e) (hello-world))))
-        (root (StackPane)))
-    (root:children:add btn)
-    (set! stage:scene (Scene root 300 250))
+  (let* ((editor (RichTextArea))
+         (root   (BorderPane editor)))
+    (set! stage:scene (Scene root 800 600))
     (stage:show)))
 
 (Platform:startup
